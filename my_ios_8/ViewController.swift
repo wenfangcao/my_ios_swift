843cd3e59d72ce204135e0bc6d673a07028bd7ea
xb <173581251@qq.com>
2018-12-19 17:55:30 +0800

    Initial Commit

---
+++ my_ios_8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my_ios_8
+//
+//  Created by xb on 2018/12/19.
+//  Copyright © 2018 xb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
